2f9b84f8b11c74987bf092e795115f3e7c83ef05
usuario <elchente23@gmail.com>
2018-04-17 22:40:54 -0600

    Custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+
 10.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus et est in ultricies. Praesent sed vehicula nunc, ornare fringilla risus. Curabitur fermentum id mauris eu laoreet. Duis dictum enim et justo molestie laoreet. Fusce ut volutpat nisi. Donec id dui sit amet ligula efficitur auctor. Nullam et nisl leo. Donec consectetur faucibus est ut congue. Cras eu mi eget lacus pretium imperdiet. Ut consectetur urna non eros vehicula consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus et est in ultricies. Praesent sed vehicula nunc, ornare fringilla risus. Curabitur fermentum id mauris eu laoreet. Duis dictum enim et justo molestie laoreet. Fusce ut volutpat nisi. Donec id dui sit amet ligula efficitur auctor. Nullam et nisl leo. Donec consectetur faucibus est ut congue. Cras eu mi eget lacus pretium imperdiet. Ut consectetur urna non eros vehicula consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        topic_id: Topic.last.id
     )
 end
 
@@ -16,10 +23,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "My great service",
+        subtitle: "Ruby on rails",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus et est in ultricies. Praesent sed vehicula nunc, ornare fringilla risus. Curabitur fermentum id mauris eu laoreet. Duis dictum enim et justo molestie laoreet. Fusce ut volutpat nisi. Donec id dui sit amet ligula efficitur auctor. Nullam et nisl leo. Donec consectetur faucibus est ut congue. Cras eu mi eget lacus pretium imperdiet. Ut consectetur urna non eros vehicula consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        main_image: "http://via.placeholder.com/600x400",
+        thumb_image: "http://via.placeholder.com/350x200"
+    )
+end
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rhoncus et est in ultricies. Praesent sed vehicula nunc, ornare fringilla risus. Curabitur fermentum id mauris eu laoreet. Duis dictum enim et justo molestie laoreet. Fusce ut volutpat nisi. Donec id dui sit amet ligula efficitur auctor. Nullam et nisl leo. Donec consectetur faucibus est ut congue. Cras eu mi eget lacus pretium imperdiet. Ut consectetur urna non eros vehicula consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         main_image: "http://via.placeholder.com/600x400",
         thumb_image: "http://via.placeholder.com/350x200"
