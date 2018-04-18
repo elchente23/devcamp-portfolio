38eb9970528d49e287d9dd750af0166a0b459abf
usuario <elchente23@gmail.com>
2018-04-17 22:57:14 -0600

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,11 @@ class Portfolio < ApplicationRecord
     scope :ruby_on_rails_portfolio_items, -> {
         where(subtitle: 'Ruby on rails')
     }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "http://via.placeholder.com/600x400"
+        self.thumb_image ||= "http://via.placeholder.com/350x200"
+    end
 end
